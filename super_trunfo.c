74990284e402db58a922698cdca3aba80a432210
Owndred <gabriell_martins13@hotmail.com>
2025-06-14 15:57:33 -0300

    Restruturação da parte de comparação das cartas. Adicionado a opção de selecionar carta com switch e opção de escolher atributos também com switch

--- super_trunfo.c
+++ super_trunfo.c
@@ -5,6 +5,7 @@ int main(){
     setlocale(LC_ALL, "Portuguese");  // comando para corrigir escrita em portugues como ð‡ e ð‰
 
     // Variaveis para carta 1
+
     char codigo_carta[4];     
     char estado[20]; 
     char nome[20];
@@ -15,19 +16,9 @@ int main(){
     float densidade_populacional;
     float pib_per_capita;
     float super_poder;
-
-
-    // Variáveis para comparaç?o
-    float comparacao_populacao;
-    float comparacao_area;
-    float comparacao_pib;
-    float comparacao_pturisticos;
-    float comparacao_super_poder;
-    float comparacao_densidade_populacional;
-    float comparacao_pib_per_capita;
-
     
     // Variaveis para carta 2
+
     char codigo_carta2[4];
     char nome2[20];
     char estado2[20];
@@ -38,7 +29,13 @@ int main(){
     float densidade_populacional2;
     float pib_per_capita2;
     float super_poder2;
+
+    // Variáveis para comparaç?o
     
+    int cartas;
+    int atributos;
+
+    // Cadastrando valores das cartas
 
     printf("Cadastre suas cartas\n");   
 
@@ -62,6 +59,8 @@ int main(){
     
     printf("Digite quantos pontos turísticos a cidade tem: \n");
     scanf("%d", &pturisticos);
+
+    // Imprimindo carta 1 com valores
 
     printf("*** Carta 1 ***\n");
     printf("Nome da cidade: %s\n", nome);
@@ -109,56 +108,227 @@ int main(){
     printf("Número de pontos turísticos: %d\n", pturisticos2);
     printf("_______________________________________________________\n");
 
-    printf("*** Calculando parâmetros***\n");
-    
+    // Calculando atributos extra de cada carta
+
+    printf("*** Calculando parâmetros***\n"); 
     
     printf("Parâmetros para a carta 1\n");
     densidade_populacional = populacao / area;
     pib_per_capita = pib / populacao;
+    super_poder = (populacao + area + pib + pturisticos);
 
     printf("A densidade populacional da carta 1 é: %f hab/km²\n", densidade_populacional);
     printf("O PIB per Capita da carta 1 é: R$ %f reais\n", pib_per_capita);
+    printf("O Super Poder da Carta 1 é: %f\n", super_poder);
+    
     printf("_______________________________________________________\n");
 
     printf("Parâmetros para a carta 2\n");
 
     densidade_populacional2 = populacao2 / area2;
     pib_per_capita2 = pib2 / populacao2;
+    super_poder2 = (populacao2 + area2 + pib2 + pturisticos2);
 
     printf("A densidade populacional da carta 2 é: %f hab/km²\n", densidade_populacional2);
     printf("O PIB per Capita da carta 2 é: R$ %f reais\n", pib_per_capita2);
-    
+    printf("O Super Poder da Carta 2 é: %f\n", super_poder2);
+
 printf("_______________________________________________________\n");
     
-    // Comparando atributos
+    // Escolhendo a carta para disputa
 
-    super_poder = (populacao + area + pib + pturisticos);
-    super_poder2 = (populacao2 + area2 + pib2 + pturisticos2);
+    printf("Escolha qual sua carta.\n");
+    printf("1. Carta 1.\n");
+    printf("2. Carta 2.\n");
+    scanf("%d", &cartas);
 
-    comparacao_super_poder = super_poder > super_poder2;
-    comparacao_populacao = populacao > populacao2;
-    comparacao_area = area > area2;
-    comparacao_pib = pib > pib2;
-    comparacao_pturisticos = pturisticos > pturisticos2;
-    comparacao_densidade_populacional = densidade_populacional > densidade_populacional2;
-    comparacao_pib_per_capita = pib_per_capita > pib_per_capita2;
+    switch (cartas)
+    {
+    case 1: // Usuário escolheu a carta 1
+        
+        printf("Escolha o atributo para comparaç?o.\n");
+        printf("1. Populaç?o.\n");
+        printf("2. Área.\n");
+        printf("3. PIB.\n");
+        printf("4. Pontos Turisticos.\n");
+        printf("5. Densidade Populacional.\n");
+        printf("6. PIB per Capita.\n");
+        printf("7. Super Poder.\n");
+        scanf("%d", &atributos);
+
+        switch (atributos)
+        {
+        case 1:
+            if (populacao > populacao2)
+            {
+                printf("Sua cidade tem a maior populaç?o!\n");
+            }
+            else{
+                printf("Sua cidade n?o tem a maior populaç?o!\n");
+            }
+            break;
+
+        case 2:
+            if (area > area2)
+            {
+                printf("Sua cidade tem a maior área!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem a maior área!\n");
+            }
+            break;
+
+        case 3:
+            if (pib > pib2)
+            {
+                printf("Sua cidade tem o maior PIB!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior PIB!\n");
+            }
+            break;
+
+        case 4:
+            if (pturisticos > pturisticos2)
+            {
+                printf("Sua cidade tem mais pontos turisticos!\n");
+            }
+            else
+            {
+                printf("Sua cidade tem menos pontos turisticos!\n");
+            }
+            break;
+
+        case 5:
+            if (densidade_populacional > densidade_populacional2)
+            {
+                printf("Sua cidade tem a maior densidade populacional!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem a maior densidade populacional!\n");
+            }
+            break;
+
+        case 6:
+            if (pib_per_capita > pib_per_capita2)
+            {
+                printf("Sua carta tem o maior PIB per Capita!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior PIB per Capita!\n");
+            }
+            break;
+
+        case 7:
+            if (super_poder > super_poder2)
+            {
+                printf("Sua cidade tem o maior super poder!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior super poder!\n");
+            }
+            break;
+        }
+        
     
-    printf("Comparaç?o entre carta 1 e carta 2.\n");
-    printf("1 para verdadeiro 0 para falso.\n");
-    printf("A carta 1 tem a maior populaç?o? - %f\n", comparacao_populacao);
-    printf("A carta 1 tem a maior área ? - %f\n", comparacao_area);
-    printf("A carta 1 tem o maior PIB? - %f\n", comparacao_pib);
-    printf("A carta 1 tem mais pontos túristicos? - %f\n", comparacao_pturisticos);
-    printf("A carta 1 tem o maior super poder? - %f\n", comparacao_super_poder);
-    printf("A carta 1 tem a maior densidade populacional? - %f\n", comparacao_densidade_populacional);
-    printf("A carta 1 tem o maior PIB per capita? - %f\n", comparacao_pib_per_capita);
-    
-    
 
 
+    case 2: // Usuário escolheu a carta 2
+        
+        printf("Escolha o atributo para comparaç?o.\n");
+        printf("1. Populaç?o.\n");
+        printf("2. Área.\n");
+        printf("3. PIB.\n");
+        printf("4. Pontos Turisticos.\n");
+        printf("5. Densidade Populacional.\n");
+        printf("6. PIB per Capita.\n");
+        printf("7. Super Poder.\n");
+        scanf("%d", &atributos);
 
-   
+        switch (atributos)
+        {
+        case 1:
+            if (populacao < populacao2)
+            {
+                printf("Sua cidade tem a maior populaç?o!\n");
+            }
+            else{
+                printf("Sua cidade n?o tem a maior populaç?o!\n");
+            }
+            break;
 
+        case 2:
+            if (area < area2)
+            {
+                printf("Sua cidade tem a maior área!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem a maior área!\n");
+            }
+            break;
 
+        case 3:
+            if (pib < pib2)
+            {
+                printf("Sua cidade tem o maior PIB!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior PIB!\n");
+            }
+            break;
+
+        case 4:
+            if (pturisticos < pturisticos2)
+            {
+                printf("Sua cidade tem mais pontos turisticos!\n");
+            }
+            else
+            {
+                printf("Sua cidade tem menos pontos turisticos!\n");
+            }
+            break;
+
+        case 5:
+            if (densidade_populacional < densidade_populacional2)
+            {
+                printf("Sua cidade tem a maior densidade populacional!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem a maior densidade populacional!\n");
+            }
+            break;
+
+        case 6:
+            if (pib_per_capita < pib_per_capita2)
+            {
+                printf("Sua carta tem o maior PIB per Capita!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior PIB per Capita!\n");
+            }
+            break;
+
+        case 7:
+            if (super_poder < super_poder2)
+            {
+                printf("Sua cidade tem o maior super poder!\n");
+            }
+            else
+            {
+                printf("Sua cidade n?o tem o maior super poder!\n");
+            }
+            break;
+        }
+
+    }
     return 0;
 }
